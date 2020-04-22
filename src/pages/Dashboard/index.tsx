9fb84a71461d0eb789890d09a3c5fb5eff2b6cec
gutovesco <guto.vesco@hotmail.com>
2020-04-22 14:28:06 -0300

    styles and dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,23 @@
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>;
+    return (
+        <>
+            <img src={logoImg} alt="logo" />
+            <Title>Explore repositórios no github</Title>
+
+            <Form action="">
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories />
+        </>
+    );
 };
 
 export default Dashboard;
